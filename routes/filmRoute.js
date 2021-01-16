ddb86c5e6ea397d957d10e59754e7679145c0fef
huskarit99 <nguyenthaihocxm1999@gmail.com>
2021-01-16 08:56:21 +0700

    ADD: future film

---
+++ routes/filmRoute.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var jsonUtils = require('../app-helpers/json_utils');
+var filmRoute = express.Router();
+var film = require('../models/film');
+
+
+filmRoute.get("/futureFilm", async function (req, res) {
+    var rec_per_page = 6;
+    var curPage = req.query.page ? req.query.page : 1;
+    var offset = (curPage - 1) * rec_per_page;
+
+    film.loadFilm(rec_per_page, offset, 1).then(function (data) {
+        var number_of_pages = data.total / rec_per_page;
+        if (data.total % rec_per_page > 0) {
+            number_of_pages++;
+        }
+
+        var pages = [];
+        for (var i = 1; i <= number_of_pages; i++) {
+            pages.push({
+                pageValue: i,
+                isActive: i === +curPage
+            });
+        }
+        res.render('film/futureFilm', {
+            layoutModels: res.locals.layoutModels,
+            isEmpty: data.rows.length === 0,
+            total: data.total,
+            rows: data.rows,
+            pages: pages,
+            curPage: curPage,
+            prevPage: curPage - 1,
+            nextPage: parseInt(curPage) + 1,
+            showPrevPage: curPage > 1,
+            showNextPage: curPage < number_of_pages - 1
+        });
+    });
+})
+
+
+module.exports = filmRoute;
